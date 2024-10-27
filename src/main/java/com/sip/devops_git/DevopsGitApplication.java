bc2e144a59c94a18cefcf1c090bba7ec85e8d03a
Marouen <marouenghnimi7@gmail.com>
2024-10-27 20:34:51 +0100

    version 0 du projet

---
+++ src/main/java/com/sip/devops_git/DevopsGitApplication.java
@@ -0,0 +1,14 @@
+package com.sip.devops_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGitApplication.class, args);
+		System.out.println("Git basics");
+	}
+
+}
